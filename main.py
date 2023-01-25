a018e194538945c36bbbf06f4a4005de3730b650
houcine <ounishoucine.on@gmail.com>
2023-01-25 02:39:39 +0100

    init commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# " hello wordl"
+print(" hello world")
+print(" hello hamza")
+# first prog FIND THE PASSWORD
+secret_code= 10
+guess_count = 0
+while guess_count < 3:
+    guess_number = int(input(" insert your code :"))
+    if guess_number == secret_code :
+        print(" you won ")
+        break
+    else:
+       print( " try a gain")
+       guess_count  +=1
