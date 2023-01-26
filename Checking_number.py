552fbac2075194808be7249931fef3b953888ea1
houcine <ounishoucine.on@gmail.com>
2023-01-26 23:27:55 +0100

    i wille remote the seconde prog

---
+++ Checking_number.py
@@ -0,0 +1,8 @@
+# task   checking number
+number =int(input(" isert you number : " ))
+if number  <  0 :
+     print(" the number is negative  ")
+elif number  > 0  :
+     print(' the number is  positive  ')
+else :
+    print(' th number is zero')
